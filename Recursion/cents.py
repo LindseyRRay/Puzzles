6deb4b0d05457c0c07c7f71b7f44f37553d0f2bf
lrraymond13 <lrraymond13@gmail.com>
2014-12-29 21:21:44 -0500

    added couple more recursion problems

--- Recursion/cents.py
+++ Recursion/cents.py
@@ -1,1 +1,29 @@
 #Q: Given an infinie number of quarters, dimes, nickels, pennies, calculate number of ways to represent n cents 
+from collections import defaultdict
+
+def calculate_change(cents):
+	if cents < 0:
+		return 0
+	elif cents == 0:
+		return 1
+	elif cents >=25:
+		return calculate_change(cents-25) + \
+		calculate_change(cents-10) + \
+		calculate_change(cents-5) + \
+		calculate_change(cents-1)
+	elif cents >= 10:
+		return calculate_change(cents-10) + \
+		calculate_change(cents-5) +\
+		calculate_change(cents-1)
+	elif cents >=5:
+		return calculate_change(cents-5) + \
+		calculate_change(cents-1)
+	else:
+		return calculate_change(cents-1)
+
+def main():
+	ways = calculate_change(50)
+	print(ways)
+
+
+main()
